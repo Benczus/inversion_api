c8e64e5976ef872fd739c3381f7eac7d2f18966b
Bence Bogdándy <bogdandy@iit.uni-miskolc.hu>
2020-07-29 17:11:16 +0200

    Created privated methods for separate functions -added basic logging -TODO: Add additional logging

--- GA_inverter.py
+++ GA_inverter.py
@@ -7,7 +7,7 @@ import pandas as pd
 from deap import base
 from deap import creator
 from deap import tools
-
+from sklearn.metrics  import r2_score
 from util import setup_logger, calculate_spherical_coordinates
 
 current_datetime = datetime.datetime.now()
@@ -36,7 +36,7 @@ class GA_Inverter():
         self.OUTPUT_TOLERANCE= OUTPUT_TOLERANCE
         self.__initialize_invertion_functions()
 
-    def creator_function(self):
+    def __creator_function(self):
         return self.creator.Individual(self._generate_individual())
 
     def _generate_individual(self):
@@ -64,7 +64,7 @@ class GA_Inverter():
         self.creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
         self.creator.create("Individual", list, fitness=creator.FitnessMin)
         # toolbox.register("attr_float", random.random())
-        self.toolbox.register("population", tools.initRepeat, list, self.creator_function, n=self.POP_SIZE)
+        self.toolbox.register("population", tools.initRepeat, list, self.__creator_function, n=self.POP_SIZE)
         self.pop = self.toolbox.population()
         self.toolbox.register("mate", tools.cxTwoPoint)
         self.toolbox.register("mutate", tools.mutShuffleIndexes)
@@ -76,52 +76,59 @@ class GA_Inverter():
     def __generate_valid_pop(self, y_predict, model, scaler):
         ga_logger.info("Started generate_valid_pop method")
         fitnesses = list()
-        # evaluation
-        for individual in self.pop:
-            temp = self.toolbox.evaluate(individual, model,
-                                         scaler.transform([[0, 0, 0, 0, 0, 0, 0, 0, self.DESIRED_OUTPUT]])[0][8])
-            fitnesses.append(temp)
-        for ind, fit in zip(self.pop, fitnesses):
-            ind.fitness.values = fit
-
+        #First pass
+        fitnesses=self.__evaluate_individuals(model, scaler.transform([[0, 0, 0, 0, 0, 0, 0, 0, self.DESIRED_OUTPUT]])[0][8] , fitnesses)
         for g in range(self.NGEN):
-
-            elits = self.toolbox.selectBest(self.pop, k=self.elite_count)
-            elits = list(map(self.toolbox.clone, elits))
+            elites = self.toolbox.selectBest(self.pop, k=self.elite_count)
+            elites = list(map(self.toolbox.clone, elites))
             offsprings = self.toolbox.selectWorst(self.pop, k=self.POP_SIZE - self.elite_count)
-
             offsprings = list(map(self.toolbox.clone, offsprings))
-
-            sumFitness = 0
-            for ind in self.pop:
-                sumFitness = sumFitness + ind.fitness.values[0]
-
-            for offspring in offsprings:
-                parents = tools.selRoulette(self.pop, 2)
-                parents = list(map(self.toolbox.clone, parents))
-                offspring = tools.cxTwoPoint(parents[0], parents[1])[0]
-                del offspring.fitness.values
-
-            # Evaluate the individuals with an invalid fitness
-            invalid_ind = [ind for ind in offsprings if not ind.fitness.valid]
-            for index, individual in enumerate(invalid_ind):
-                fitnesses.append(self.toolbox.evaluate(individual, model, y_predict[index]))
-            for ind, fit in zip(invalid_ind, fitnesses):
-                ind.fitness.values = fit
-
-            for i in range(self.elite_count):
-                self.pop[i] = elits[i]
-            for i in range(self.POP_SIZE - self.elite_count):
-                self.pop[i + self.elite_count] = offsprings[i]
-
-            for index, individual in enumerate(self.pop):
-                temp = self.toolbox.evaluate(individual, model, y_predict[index])
-            fitnesses.append(temp)
-            for ind, fit in zip(self.pop, fitnesses):
-                ind.fitness.values = fit
+            parents, offsprings=self.__generate_offspings(offsprings)
+            fitnesses, invalid_ind = self.__evaluate_invalid_individuals(offsprings, fitnesses)
+            self.__create_new_generation(elites,offsprings)
+            fitnesses=self.__evaluate_individuals(model, y_predict, fitnesses)
         ga_logger.info("Done generate_valid_pop method")
+        ga_logger.debug(self.pop)
         return [ind for ind in self.pop if ind.fitness.values[0] < 2]
 
+    def __create_new_generation(self, elites, offsprings):
+        ga_logger.info("Started __create_new_generation method")
+        for i in range(self.elite_count):
+            self.pop[i] = elites[i]
+        for i in range(self.POP_SIZE - self.elite_count):
+            self.pop[i + self.elite_count] = offsprings[i]
+        ga_logger.info("Done __create_new_generation method")
+
+    def __generate_offspings(self, offsprings):
+        ga_logger.info("Started __generate_offspings method")
+        for index, offspring in enumerate(offsprings):
+            parents = tools.selRoulette(self.pop, 2)
+            parents = list(map(self.toolbox.clone, parents))
+            offsprings[index] = tools.cxTwoPoint(parents[0], parents[1])[0]
+            del offspring.fitness.values
+        ga_logger.info("Done __generate_offspings method")
+        return parents, offsprings
+
+
+    def __evaluate_invalid_individuals(self, offsprings, model, outcome, fitnesses):
+        ga_logger.info("Started __evaluate_invalid_individuals method")
+        invalid_ind = [ind for ind in offsprings if not ind.fitness.valid]
+        for index, individual in enumerate(invalid_ind):
+            fitnesses.append(self.toolbox.evaluate(individual, model, outcome))
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+        ga_logger.info("Done __evaluate_invalid_individuals method")
+        return fitnesses, invalid_ind
+
+    def __evaluate_individuals(self,  model, outcome, fitnesses ):
+        ga_logger.info("Started __evaluate_individuals method")
+        for index, individual in enumerate(self.pop):
+            temp = self.toolbox.evaluate(individual, model, outcome[index])
+        fitnesses.append(temp)
+        for ind, fit in zip(self.pop, fitnesses):
+            ind.fitness.values = fit
+        ga_logger.info("Done __evaluate_individuals method")
+        return fitnesses
 
     def invert(self, y_pred,  scaler,  model):
         ga_logger.info("Started invert method")
