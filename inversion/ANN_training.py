36cdd43585bff38e323d07ee28dcdd615cff8f88
Bence Bogdándy <bogdandy.bence@uni-eszterhazy.hu>
2020-08-05 13:07:50 +0200

    - Pickled separate ann models for access points, saved them in ann_models directory in models. - added these models to gitignore - copied 3 trained model pickles into ann_test - replaced console print with debug logging - added info logging before and after ann training and pickling

--- inversion/ANN_training.py
+++ inversion/ANN_training.py
@@ -1,3 +1,5 @@
+import os
+import pickle
 from datetime import datetime
 
 from sklearn.model_selection import train_test_split
@@ -17,6 +19,7 @@ def create_ANN_list(df_list, target_list):
     ANN_List = []
     for (testDataFrame, target) in zip(df_list, target_list):
         x_train, x_test, y_train, y_test = train_test_split(testDataFrame, target)
+        ann_logger.info("Starting training for the {}".format(target.name))
         model = MLPRegressor(activation='relu', alpha=0.001, batch_size='auto', beta_1=0.9,
                              beta_2=0.999, early_stopping=False, epsilon=1e-08,
                              hidden_layer_sizes=(200, 300, 400, 300, 200), learning_rate='adaptive',
@@ -25,7 +28,17 @@ def create_ANN_list(df_list, target_list):
                              random_state=None, shuffle=True, solver='adam', tol=0.0001,
                              validation_fraction=0.1, verbose=False, warm_start=False)
         model.fit(x_train, y_train)
+        ann_logger.info("Finished  training for {}".format(target.name))
+        ann_logger.info("Saving model for  {}".format(target.name))
+        if not os.path.exists('models/ann_models'):
+             os.makedirs('models/ann_models')
+        loc="model/ann_models/{}".format(target.name)
+        with open(loc, "wb") as fp:
+             pickle.dump(model, fp)
+        ann_logger.info("Model pickling for  {} complete!".format(target.name))
+        ann_logger.debug("Model score of {} : {}".format(target.name, model.score(x_test, y_test)))
         ANN_List.append(model)
-        print(model.score(x_test, y_test))
     ann_logger.info("Done create_ANN_list method")
     return ANN_List
+
+
