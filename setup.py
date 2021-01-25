87ad8060846078060020d61854ecaa5653c89420
bogdandy <bence.bogdandy@gmail.com>
2021-01-25 13:14:45 +0100

    fixed package name in setup.py ann-inverter cannot be imported because - is an invalid symbol renamed into anninverter

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 import setuptools
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ann-inverter",
+    name="anninverter",
     version="0.0.1",
     author="bence bogdandy, zsolt toth",
     author_email="{bogdandy.bence,zsolt.toth}@uni-eszterhazy.hu",
